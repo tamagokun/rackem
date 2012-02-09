c3a3ec8f6ac059eb3524f95808c8904900c68af8
Blake Tereau <fewkeep@gmail.com>
2012-02-09 11:16:14 -0500

    made a simple app

--- test/app.php
+++ test/app.php
@@ -1,9 +1,27 @@
 <?php
-
 require __DIR__."/../autoload.php";
 
-$app = function($env) {
-	return array(200, array('Content-Type' => 'text/html'), array('Hello World!'));
-};
+session_start();
+
+function multiplication_table($n)
+	{
+	return '<table>'
+	. implode(array_map(function($i) use($n) { return table_row($i, $n); }, range(1, $n)))
+	. '</table>';
+	}
+	
+function table_row($i, $n)
+	{
+	return '<tr>'
+	. implode(array_map(function($j) use($i) { return table_cell($i * $j); }, range(1, $n)))
+	. '</tr>';
+	}
+	
+function table_cell($i)
+	{
+	return '<td>' . $i . '</td>';
+	}
+	
+$app = function() { return array(200, array(), array(multiplication_table(10))); };
 
 \Rackem\Rack::run($app);
