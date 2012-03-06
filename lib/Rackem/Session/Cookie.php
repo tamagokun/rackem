40b2ace68a9a8947005bed56298b799f532784df
Mike Kruk <mike.kruk@mslgroup.com>
2012-03-06 12:29:48 -0500

    Cookie extends Id

--- lib/Rackem/Session/Cookie.php
+++ lib/Rackem/Session/Cookie.php
@@ -1,15 +1,15 @@
 <?php
 namespace Rackem;
 
-class Cookie
+class Cookie extends Id
 {
-	public $app,$secret,$coder;
+	public $secret,$coder;
 
 	public function __construct($app, $options=array())
 	{
-		$this->app = $app;
 		$this->secret = isset($options["secret"])? $options["secret"] : "";
 		$this->coder = isset($options["coder"])? $options["coder"] : "base64";
+		parent::__construct($app,$options);
 	}
 
 	public function set_cookie($env, $headers, $cookie)
