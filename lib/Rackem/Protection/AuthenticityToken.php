6759b1dd51458327d46abd4a048a6a5acb9d3c96
Mike Kruk <mike.kruk@mslgroup.com>
2012-05-07 11:17:55 -0400

    parse error

--- lib/Rackem/Protection/AuthenticityToken.php
+++ lib/Rackem/Protection/AuthenticityToken.php
@@ -8,7 +8,7 @@ class AuthenticityToken extends \Rackem\Protection
 		if($this->is_safe($env)) return true;
 		$session = $this->session($env);
 		$token = isset($session['csrf.token'])? $session['csrf.token'] : false;
-		if(!$token && isset($session['csrf']) $token = $session['csrf'];
+		if(!$token && isset($session['csrf'])) $token = $session['csrf'];
 		if(!$token && isset($session['_csrf_token'])) $token = $session['_csrf_token'];
 		if($env['HTTP_X_CSRF_TOKEN'] == $token) return true;
 		$request = new \Rackem\Request($env);
