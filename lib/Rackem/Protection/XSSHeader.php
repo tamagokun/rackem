4aeaa8a5ecb0216a0f31834542341188c13d017a
Mike Kruk <mike.kruk@mslgroup.com>
2012-05-07 11:32:06 -0400

    fix case where headers might be null

--- lib/Rackem/Protection/XSSHeader.php
+++ lib/Rackem/Protection/XSSHeader.php
@@ -11,6 +11,6 @@ class XSSHeader extends \Rackem\Protection
 	public function call($env)
 	{
 		list($status,$headers,$body) = $this->app->call($env);
-		return array($status,array_merge($headers,$this->header()),$body);
+		return array($status,array_merge($headers? $headers : array(),$this->header()),$body);
 	}
 }
