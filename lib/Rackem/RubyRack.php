0f33fefd45f5e7107bdb22589478a78f46751b93
Mike Kruk <mike.kruk@mslgroup.com>
2012-05-10 16:00:47 -0400

    fixed a bug where some header values were being sent through as int rather than string.

--- lib/Rackem/RubyRack.php
+++ lib/Rackem/RubyRack.php
@@ -11,6 +11,7 @@ class RubyRack extends Rack
 	protected static function execute($result, $env)
 	{
 		list($status, $headers, $body) = $result;
+		foreach($headers as $k=>&$v) if(is_numeric($v)) $v = (string)$v;
 		exit(json_encode(array($status, $headers, $body)));
 	}
 	
