007df985a148f4871ff02db0aca0c935d8a397db
Mike Kruk <mike.kruk@mslgroup.com>
2012-05-11 16:13:11 -0400

    patch RubyRack so we can serve file data

--- lib/Rackem/RubyRack.php
+++ lib/Rackem/RubyRack.php
@@ -13,7 +13,11 @@ class RubyRack extends Rack
 	{
 		list($status, $headers, $body) = $result;
 		foreach($headers as $k=>&$v) if(is_numeric($v)) $v = (string)$v;
-		exit(json_encode(array($status, $headers, $body)));
+		//exit(json_encode(array($status, $headers, $body)));
+		//Temporary fix for ruby bridge so we can serve data other than UTF-8 (files)
+		$headers = json_encode($headers);
+		$body = implode("",$body);
+		exit(implode("\n",array($status,$headers,$body)));
 	}
 	
 	public static function run($app = null)
