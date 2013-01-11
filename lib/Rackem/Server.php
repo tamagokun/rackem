8753b3d97e2bd8588fa8289c7afb303517f4da8b
Mike Kruk <tamagokun@gmail.com>
2013-01-11 08:48:47 -0500

    server can properly parse all request parts, and properly write an http response.

--- lib/Rackem/Server.php
+++ lib/Rackem/Server.php
@@ -16,14 +16,18 @@ class Server
 			exit($errno > 0? $errno : 2);
 		}
 		stream_set_blocking($this->master, 0);
-		echo "== Rackem on http://{$this->host}:{$this->port}\n";
-		echo ">> Rackem web server\n";
-		echo ">> Listeneing on {$this->host}:{$this->port}, CTRL+C to stop\n";
+		declare(ticks=1);
+		pcntl_signal(SIGINT, array($this, "stop"));
+		pcntl_signal(SIGTERM, array($this, "stop"));
 	}
 
 	public function start($app)
 	{
-		while ($client = stream_socket_accept($this->master))
+		echo "== Rackem on http://{$this->host}:{$this->port}\n";
+		echo ">> Rackem web server\n";
+		echo ">> Listening on {$this->host}:{$this->port}, CTRL+C to stop\n";
+
+		while ($client = @stream_socket_accept($this->master))
 		{
 			$buffer = '';
 
@@ -31,19 +35,72 @@ class Server
 			{
 				$buffer .= fread($client, 2046);
 			}
-			$request = $this->parse_request($buffer);
+			$req = $this->parse_request($buffer);
 
-			//print_r($request);
+			$env = $this->env($req);
+			$res = new Response($app->call($env));
 
-			$env = $this->env($request);
-			list($status, $headers, $body) = $app->call($env);
-
-			//fwrite($client, $response);
+			fwrite($client, $this->write_response($req, $res));
 			fclose($client);
 		}
+		fclose($this->master);
+	}
+
+	public function stop()
+	{
+		echo ">> Stopping ...\n";
+		fclose($this->master);
+		exit(0);
 	}
 
 /* private */
+
+	protected function env($req)
+	{
+		$env = array(
+			'REQUEST_METHOD' => $req['method'],
+			'SCRIPT_NAME' => "",
+			'PATH_INFO' => $req['request_url']['path'],
+			'SERVER_NAME' => $this->host,
+			'SERVER_PORT' => $this->port,
+			'QUERY_STRING' => $req['request_url']['query']
+		);
+		return new \ArrayObject($env);
+	}
+
+	protected function get_url_parts($request, $parts)
+	{
+		$url = array(
+			'path'   => $request,
+			'scheme' => 'http',
+			'host'   => '',
+			'port'   => '',
+			'query'  => ''
+		);
+
+		if(isset($parts['headers']['Host']))
+			$url['host'] = $parts['headers']['Host'];
+		elseif(isset($parts['headers']['host']))
+			$url['host'] = $parts['headers']['host'];
+		
+		if(strpos($url['host'], ':') !== false)
+		{
+			$host = explode(':', $url['host']);
+			$url['host'] = trim($host[0]);
+			$url['port'] = (int) trim($host[1]);
+			if($url['port'] == 443) $url['scheme'] = 'https';
+		}
+
+		$path  = $url['path'];
+		$query = strpos($path, '?');
+		if($query)
+		{
+			$url['query'] = substr($path, $query + 1);
+			$url['path'] = substr($path, 0, $query);
+		}
+
+		return $url;
+	}
 
 	protected function parse_parts($req)
 	{
@@ -110,22 +167,23 @@ class Server
 			'body'     => $parts['body']
 		);
 
-		//$parsed['request_url'] = $this->get_url_parts($parts['start'][1], $parsed);
+		$parsed['request_url'] = $this->get_url_parts($parts['start'][1], $parsed);
 
 		return $parsed;
 	}
 
-	protected function env($req)
+	protected function write_response($req, $res)
 	{
-		$headers = $req->getHeaders();
-		$env = array(
-			'REQUEST_METHOD' => $req->getMethod(),
-			'SCRIPT_NAME' => "",
-			'PATH_INFO' => $req->getPath(),
-			'SERVER_NAME' => $this->host,
-			'SERVER_PORT' => $this->port,
-			'QUERY_STRING' => $req->getQuery()
-		);
-		return new \ArrayObject($env);
+		list($status, $headers, $body) = $res->finish();
+		$phrase = Utils::status_code($status);
+		$body = implode("", $body);
+		$head = "{$req['protocol']}/{$req['version']} $status $phrase";
+
+		$raw_headers = array();
+		foreach($headers as $k=>$v) $raw_headers[] = "$k: $v";
+
+		$head .= implode("\r\n", $raw_headers);
+		return "$head\r\n\r\n$body";
 	}
+
 }
