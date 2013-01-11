b2b4a035ba5b04f231f28bfbd22595fe13b12645
Mike Kruk <tamagokun@gmail.com>
2013-01-10 19:43:04 -0500

    basic request handling and parsing without the use of other libraries.

--- lib/Rackem/Server.php
+++ lib/Rackem/Server.php
@@ -9,35 +9,111 @@ class Server
 	{
 		$this->host = $host;
 		$this->port = $port;
+		$this->master = @stream_socket_server("tcp://$host:$port", $errno, $errstr);
+		if($this->master === false)
+		{
+			echo ">> Failed to start server.\n";
+			exit($errno > 0? $errno : 2);
+		}
+		stream_set_blocking($this->master, 0);
+		echo "== Rackem on http://{$this->host}:{$this->port}\n";
+		echo ">> Rackem web server\n";
+		echo ">> Listeneing on {$this->host}:{$this->port}, CTRL+C to stop\n";
 	}
 
 	public function start($app)
 	{
-		$loop = \React\EventLoop\Factory::create();
-		$this->socket = new \React\Socket\Server($loop);
-
-		$http = new \React\Http\Server($this->socket);
-		$http->on('request', function($req,$res) use($app) {
-			$env = $this->env($req);
-			list($status, $headers, $body) = $app->call($env);
-			$res->writeHead($status, $headers);
-			$res->end(implode("\n",$body));
-		});
-		
-		try {
-			$this->socket->listen($this->port);
-			echo "== Rackem on http://{$this->host}:{$this->port}/\n";
-			echo ">> Rackem web server\n";
-			echo ">> Listening on {$this->host}:{$this->port}, CTRL+C to stop\n";
-			$loop->run();
-		}catch(Exception $e)
+		while ($client = stream_socket_accept($this->master))
 		{
-			echo ">> Failed to start server.";
-			exit(1);
+			$buffer = '';
+
+			while (!preg_match('/\r?\n\r?\n/', $buffer))
+			{
+				$buffer .= fread($client, 2046);
+			}
+			$request = $this->parse_request($buffer);
+
+			//print_r($request);
+
+			$env = $this->env($request);
+			list($status, $headers, $body) = $app->call($env);
+
+			//fwrite($client, $response);
+			fclose($client);
 		}
 	}
 
 /* private */
+
+	protected function parse_parts($req)
+	{
+		$start = null;
+		$headers = array();
+		$body = '';
+
+		$lines = preg_split('/(\\r?\\n)/',$req, -1, PREG_SPLIT_DELIM_CAPTURE);
+		for($i=0, $total = count($lines); $i < $total; $i += 2)
+		{
+			$line = $lines[$i];
+			if(empty($line))
+			{
+				if($i < $total - 1) $body = implode('', array_slice($lines, $i + 2));
+				break;
+			}
+
+			if(!$start)
+			{
+				$start = explode(' ', $line, 3);
+			}elseif(strpos($line, ':'))
+			{
+				$parts = explode(':', $line, 2);
+				$key = trim($parts[0]);
+				$value = isset($parts[1])? trim($parts[1]) : '';
+				if(!isset($headers[$key]))
+					$headers[$key] = $value;
+				elseif(!is_array($headers[$key]))
+					$headers[$key] = array($headers[$key], $value);
+				else
+					$headers[$key][] = $value;
+			}
+		}
+
+		return array(
+			'start'   => $start,
+			'headers' => $headers,
+			'body'    => $body
+		);
+	}
+
+	protected function parse_request($raw)
+	{
+		if(!$raw) return false;
+
+		$parts = $this->parse_parts($raw);
+
+		if(isset($parts['start'][2]))
+		{
+			$start = explode('/', $parts['start'][2]);
+			$protocol = strtoupper($start[0]);
+			$version = isset($start[1])? $start[1] : '1.1';
+		}else
+		{
+			$protocol = 'HTTP';
+			$version = '1.1';
+		}
+
+		$parsed = array(
+			'method'   => strtoupper($parts['start'][0]),
+			'protocol' => $protocol,
+			'version'  => $version,
+			'headers'  => $parts['headers'],
+			'body'     => $parts['body']
+		);
+
+		//$parsed['request_url'] = $this->get_url_parts($parts['start'][1], $parsed);
+
+		return $parsed;
+	}
 
 	protected function env($req)
 	{
