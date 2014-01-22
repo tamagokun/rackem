9fa87e00477fc900f13d28fbf550e72756ae8cc8
Mike Kruk <tamagokun@gmail.com>
2014-01-21 20:20:24 -0500

    fixed issues where responses would finish without writing all of their body.

--- lib/Rackem/Server.php
+++ lib/Rackem/Server.php
@@ -5,192 +5,176 @@ use \Rackem\Server\Connection;
 
 class Server
 {
-  public $app, $reload = true;
-  private $host, $port, $running, $in, $out;
+    public $app, $reload = true;
+    private $host, $port, $running, $in, $out;
 
-  public function __construct($host = '0.0.0.0', $port = 9393, $app = 'config.php')
-  {
-    declare(ticks=1);
-    $this->host = $host;
-    $this->port = $port;
-    $this->app = $app;
-    $this->in = array();
-    $this->out = array();
-
-    \Rackem::$handler = new \Rackem\Handler\Rackem();
-  }
-
-  public function start()
-  {
-    $this->init();
-    $this->running = true;
-
-    while($this->step())
+    public function __construct($host = '0.0.0.0', $port = 9393, $app = 'config.php')
     {
-
-    }
-  }
-
-  public function step()
-  {
-    $read = array();
-    $write = array();
-    $except = null;
-
-    foreach($this->in as $id => $conn)
-    {
-      if(!$conn->is_request_complete())
-      {
-        $read[] = $conn->socket;
-      }else
-      {
-        if(strlen($conn->buffer) || $conn->is_response_complete()) $write[] = $conn->socket;
-        else $read[] = $conn->stream;
-      }
-    }
-    $read[] = $this->master;
-
-    if(@stream_select($read, $write, $except, null) < 1) return $this->running;
-
-    if(in_array($this->master, $read))
-    {
-      $client = stream_socket_accept($this->master);
-      $this->in[(int)$client] = new Connection($client);
-
-      $key = array_search($this->master, $read);
-      unset($read[$key]);
+        declare(ticks=1);
+        $this->host = $host;
+        $this->port = $port;
+        $this->app = $app;
+        $this->in = array();
+        $this->out = array();
     }
 
-    foreach($read as $stream)
+    public function start()
     {
-      if(isset($this->out[(int)$stream])) $this->out[(int)$stream]->read();
-      else $this->read_request($stream);
+        $this->init();
+        $this->running = true;
+
+        while($this->step()) { /*BOOM*/ }
     }
 
-    foreach($write as $client) $this->write($client);
-
-    return $this->running;
-  }
-
-  public function read_request($socket)
-  {
-    $conn = $this->in[(int)$socket];
-    $data = @fread($socket, 30000);
-
-    if($data === false || $data == '') return $this->close_connection($conn);
-
-    $conn->data($data);
-    if($conn->is_request_complete())
+    public function step()
     {
-      $stream = $conn->process($this->app);
-      $this->out[(int)$stream] = $conn;
+        $read = array();
+        $write = array();
+        $except = null;
+
+        foreach($this->in as $id => $conn) {
+            if(!$conn->is_request_complete()) {
+                $read[] = $conn->socket;
+            } else {
+                if($conn->socket) $write[] = $conn->socket;
+                if(!$conn->is_response_complete()) $read[] = $conn->stream;
+            }
+        }
+        $read[] = $this->master;
+
+        if(@stream_select($read, $write, $except, null) < 1) return $this->running;
+
+        if(in_array($this->master, $read)) {
+            $client = stream_socket_accept($this->master);
+            $this->in[(int)$client] = new Connection($client);
+
+            $key = array_search($this->master, $read);
+            unset($read[$key]);
+        }
+
+        foreach($read as $stream) {
+            if(isset($this->out[(int)$stream])) $this->out[(int)$stream]->read();
+            else $this->read_request($stream);
+        }
+
+        foreach($write as $client) $this->write($client);
+
+        return $this->running;
     }
-  }
 
-  public function write($socket)
-  {
-    $conn = $this->in[(int)$socket];
-
-    $bytes = @fwrite($socket, $conn->buffer);
-    if($bytes === false) return $this->close_connection($conn);
-
-    $conn->bytes_written += $bytes;
-    $conn->buffer = substr($conn->buffer, $bytes);
-
-    if($conn->is_response_complete()) $this->complete_response($conn);
-  }
-
-  public function complete_response($conn)
-  {
-    fwrite(STDERR, $this->log_request($conn));
-    if($conn->get_header('Connection') === 'close' || $conn->version !== 'HTTP/1.1')
+    public function read_request($socket)
     {
-      $this->close_connection($conn);
-    }else
-    {
-      $conn->cleanup();
-      $this->close_response($conn);
-      $this->in[(int)$conn->socket] = new Connection($conn->socket);
+        $conn = $this->in[(int)$socket];
+        $data = @fread($socket, 30000);
+
+        if($data === false || $data == '') return $this->close_connection($conn);
+
+        $conn->data($data);
+        if($conn->is_request_complete()) {
+            $conn->process($this->app);
+            $this->out[(int)$conn->stream] = $conn;
+        }
     }
-  }
 
-  public function close_connection($conn)
-  {
-    $conn->cleanup();
-    @fclose($conn->socket);
-    unset($this->in[(int)$conn->socket]);
-    $conn->socket = null;
-
-    $this->close_response($conn);
-  }
-
-  public function close_response($conn)
-  {
-    if(!$conn->stream) return;
-    @fclose($conn->stream);
-    unset($this->out[(int)$conn->stream]);
-    $conn->stream = null;
-    if($conn->proc)
+    public function write($socket)
     {
-      proc_close($conn->proc);
-      $conn->proc = null;
+        $conn = $this->in[(int)$socket];
+
+        $bytes = @fwrite($socket, $conn->buffer);
+        if($bytes === false) return $this->close_connection($conn);
+
+        $conn->bytes_written += $bytes;
+        $conn->buffer = substr($conn->buffer, $bytes);
+        if($conn->bytes_written < $conn->response_length) return;
+        if($conn->is_response_complete()) $this->complete_response($conn);
     }
-  }
 
-  public function stop()
-  {
-    $this->running = false;
-    fclose($this->master);
-    echo ">> Stopping...\n";
-    exit(0);
-  }
+    public function complete_response($conn)
+    {
+        fwrite(STDERR, $this->log_request($conn));
+        if($conn->get_header('Connection') === 'close' || $conn->version !== 'HTTP/1.1') {
+            $this->close_connection($conn);
+        } else {
+            $conn->cleanup();
+            $this->close_response($conn);
+            $this->in[(int)$conn->socket] = new Connection($conn->socket);
+        }
+    }
 
-  public function help()
-  {
-    echo "Usage:\n";
-    echo "rackem [options] [config]\n\n";
-    echo "Options:\n";
-    echo "        --basic        run a basic HTTP server\n";
-    echo "        --host HOST    listen on HOST (default: 127.0.0.1)\n";
-    echo "        --port PORT    use PORT (default: 9393)\n";
-    echo "    -h                 show this message\n";
-    echo "    -v, --version      show version.\n";
-  }
+    public function close_connection($conn)
+    {
+        $conn->cleanup();
+        @fclose($conn->socket);
+        unset($this->in[(int)$conn->socket]);
+        $conn->socket = null;
 
-  public function version()
-  {
-    echo explode(".", \Rackem::version()) ."\n";
-  }
+        $this->close_response($conn);
+    }
+
+    public function close_response($conn)
+    {
+        if(!$conn->stream) return;
+        @fclose($conn->stream);
+        unset($this->out[(int)$conn->stream]);
+        $conn->stream = null;
+        if($conn->proc) {
+            proc_close($conn->proc);
+            $conn->proc = null;
+        }
+    }
+
+    public function stop()
+    {
+        $this->running = false;
+        fclose($this->master);
+        echo ">> Stopping...\n";
+        exit(0);
+    }
+
+    public function help()
+    {
+        echo "Usage:\n";
+        echo "rackem [options] [config]\n\n";
+        echo "Options:\n";
+        echo "        --basic        run a basic HTTP server\n";
+        echo "        --host HOST    listen on HOST (default: 127.0.0.1)\n";
+        echo "        --port PORT    use PORT (default: 9393)\n";
+        echo "    -h                 show this message\n";
+        echo "    -v, --version      show version.\n";
+    }
+
+    public function version()
+    {
+        echo explode(".", \Rackem::version()) ."\n";
+    }
 
 /* private */
 
-  protected function init()
-  {
-    set_time_limit(0);
-    $this->master = @stream_socket_server("tcp://{$this->host}:{$this->port}", $errno, $errstr);
-    if($this->master === false)
+    protected function init()
     {
-      echo ">> Failed to bind socket.\n", $errno, " - ", $errstr, "\n";
-      exit(2);
-    }
-    stream_set_blocking($this->master, 0);
+        set_time_limit(0);
+        $this->master = @stream_socket_server("tcp://{$this->host}:{$this->port}", $errno, $errstr);
+        if($this->master === false) {
+            echo ">> Failed to bind socket.\n", $errno, " - ", $errstr, "\n";
+            exit(2);
+        }
+        stream_set_blocking($this->master, 0);
 
-    echo "== Rack'em on http://{$this->host}:{$this->port}\n";
-    echo ">> Rack'em web server\n";
-    echo ">> Listening on {$this->host}:{$this->port}, CTRL+C to stop\n";
-    if(function_exists('pcntl_signal'))
+        echo "== Rack'em on http://{$this->host}:{$this->port}\n";
+        echo ">> Rack'em web server\n";
+        echo ">> Listening on {$this->host}:{$this->port}, CTRL+C to stop\n";
+        if(function_exists('pcntl_signal')) {
+            pcntl_signal(SIGINT, array($this, "stop"));
+            pcntl_signal(SIGTERM, array($this, "stop"));
+        }
+    }
+
+    protected function log_request($conn)
     {
-      pcntl_signal(SIGINT, array($this, "stop"));
-      pcntl_signal(SIGTERM, array($this, "stop"));
+        $date = @date("d/M/Y H:i:s");
+        $time = sprintf('%.4f', microtime(true) - $conn->start_time);
+
+        return "{$conn->client} - - [{$date}] \"{$conn->request}\" {$conn->status} - {$time}\n";
     }
-  }
-
-  protected function log_request($conn)
-  {
-    $date = @date("d/M/Y H:i:s");
-    $time = sprintf('%.4f', microtime(true) - $conn->start_time);
-
-    return "{$conn->client} - - [{$date}] \"{$conn->request}\" {$conn->status} - {$time}\n";
-  }
 
 }
--- lib/Rackem/Server/Connection.php
+++ lib/Rackem/Server/Connection.php
@@ -3,214 +3,219 @@ namespace Rackem\Server;
 
 class Connection
 {
-  public $env;
+    public $env;
 
-  // processing requests
-  public $client;
-  public $socket;
-  public $state = 0;
-  public $header = array();
-  public $body;
-  public $request;
-  public $content_length = 0;
-  public $version;
-  public $start_time;
+    // processing requests
+    public $client;
+    public $socket;
+    public $state = 0;
+    public $header = array();
+    public $body;
+    public $request;
+    public $content_length = 0;
+    public $version;
+    public $start_time;
 
-  protected $bytes_read = 0;
-  protected $header_buffer = '';
-  protected $is_chunked = false;
+    protected $bytes_read = 0;
+    protected $header_buffer = '';
+    protected $is_chunked = false;
 
-  // processing responses
-  public $buffer = '';
-  public $bytes_written = 0;
-  public $proc = null;
-  public $stream = null;
-  public $status = null;
+    // processing responses
+    public $buffer = '';
+    public $bytes_written = 0;
+    public $response_length = 0;
+    public $proc = null;
+    public $stream = null;
+    public $status = null;
 
-  const READ_CHUNK_HEADER = 0;
-  const READ_CHUNK_DATA = 1;
-  const READ_CHUNK_TRAILER = 2;
+    const READ_CHUNK_HEADER = 0;
+    const READ_CHUNK_DATA = 1;
+    const READ_CHUNK_TRAILER = 2;
 
-  const READ_HEADERS = 0;
-  const READ_CONTENT = 1;
-  const READ_COMPLETE = 2;
+    const READ_HEADERS = 0;
+    const READ_CONTENT = 1;
+    const READ_COMPLETE = 2;
 
-  public function __construct($client)
-  {
-    $this->socket = $client;
-    $this->body = fopen("data://text/plain,", "r+b");
+    public function __construct($client)
+    {
+        $this->socket = $client;
+        $this->body = fopen("data://text/plain,", "r+b");
 
-    stream_set_blocking($this->socket, 0);
-    $client = stream_socket_get_name($this->socket, false);
-    if(strpos($client, ':') !== false) list($client, $port) = explode(':', $client, 2);
-    $this->client = $client;
-  }
+        stream_set_blocking($this->socket, 0);
+        $client = stream_socket_get_name($this->socket, false);
+        if(strpos($client, ':') !== false) list($client, $port) = explode(':', $client, 2);
+        $this->client = $client;
+    }
 
-  public function cleanup()
-  {
-    @fclose($this->body);
-    $this->body = null;
-  }
+    public function cleanup()
+    {
+        @fclose($this->body);
+        $this->body = null;
+    }
 
-  public function is_request_complete()
-  {
-    return $this->state == Connection::READ_COMPLETE;
-  }
+    public function is_request_complete()
+    {
+        return $this->state == Connection::READ_COMPLETE;
+    }
 
     public function is_response_complete()
     {
         if( is_null($this->proc) ) return false;
         $status = proc_get_status($this->proc);
-        return $status['running'] ? false : true;
+        if($status['running']) return false;
+        if(!feof($this->stream)) return false;
+        return true;
     }
 
-  public function get_header($key)
-  {
-    return isset($this->header[$key]) ? $this->header[$key][0] : "";
-  }
-
-  /*
-   * Append data from request
-   */
-  public function data($data)
-  {
-    if($this->state == Connection::READ_HEADERS)
+    public function get_header($key)
     {
-      if(!$this->start_time) $this->start_time = microtime(true);
-      $this->header_buffer .= $data;
-
-      $end = strpos($this->header_buffer, "\r\n\r\n", 4);
-      if($end === false) return;
-
-      // parse HTTP request
-      $this->parse_request(substr($this->header_buffer, 0, $end));
-
-      // TODO: check for Transfer-Encoding: chunked
-
-      $this->content_length = (int)$this->get_header('Content-Length');
-
-      $body_start = $end + 4;
-      $data = substr($this->header_buffer, $body_start);
-
-      $this->state = Connection::READ_CONTENT;
+        return isset($this->header[$key]) ? $this->header[$key][0] : "";
     }
 
-    if($this->state == Connection::READ_CONTENT)
+    /*
+    * Append data from request
+    */
+    public function data($data)
     {
-      if($this->is_chunked)
-      {
-        // TODO: Handle chunked data
-      }else
-      {
-        fwrite($this->body, $data);
-        $this->bytes_read += strlen($data);
+        if($this->state == Connection::READ_HEADERS) {
+            if(!$this->start_time) $this->start_time = microtime(true);
+            $this->header_buffer .= $data;
 
-        if($this->content_length - $this->bytes_read <= 0) $this->state = Connection::READ_COMPLETE;
-      }
+            $end = strpos($this->header_buffer, "\r\n\r\n", 4);
+            if($end === false) return;
+
+            // parse HTTP request
+            $this->parse_request(substr($this->header_buffer, 0, $end));
+
+            // TODO: check for Transfer-Encoding: chunked
+
+            $this->content_length = (int)$this->get_header('Content-Length');
+
+            $body_start = $end + 4;
+            $data = substr($this->header_buffer, $body_start);
+
+            $this->state = Connection::READ_CONTENT;
+        }
+
+        if($this->state == Connection::READ_CONTENT) {
+            if($this->is_chunked) {
+                // TODO: Handle chunked data
+            } else {
+                fwrite($this->body, $data);
+                $this->bytes_read += strlen($data);
+
+                if($this->content_length - $this->bytes_read <= 0) $this->state = Connection::READ_COMPLETE;
+            }
+        }
+
+        if($this->state == Connection::READ_COMPLETE) fseek($this->body, 0);
     }
 
-    if($this->state == Connection::READ_COMPLETE) fseek($this->body, 0);
-  }
-
-  /*
-   * Read data from Rack'em response
-   */
-  public function read()
-  {
-    $data = @fread($this->stream, 30000);
-
-    if($data === false) return;
-
-    $this->buffer .= $data;
-
-    if(!is_null($this->status)) return;
-    $end = strpos($this->buffer, "\r\n");
-    if($end === false) return;
-    list($version, $status, $phrase) = explode(' ', substr($this->buffer, 0, $end), 3);
-    $this->status = $status;
-  }
-
-  /*
-   * Begin response for request.
-   */
-  public function process($app)
-  {
-    $spec = array(
-      0 => $this->body,
-      1 => array("pipe", "wb"),
-      2 => STDERR
-    );
-
-    $env = array();
-    // bring over standard $_SERVER array
-    foreach($_SERVER as $k=>$v) if(is_numeric($v) || is_string($v)) $env[$k] = $v;
-    $env = array_merge($env, $this->env);
-    foreach($this->header as $k=>$v)
+    /*
+    * Read data from Rack'em response
+    */
+    public function read()
     {
-      $env_name = str_replace("-", "_", strtoupper($k));
-      if(!isset($this->env[$env_name]))
-        $env["HTTP_$env_name"] = is_array($v) ? implode("\n", $v) : $v;
+        $data = @fread($this->stream, 30000);
+
+        if($data === false) return;
+        $this->buffer .= $data;
+
+        if(!is_null($this->status)) return;
+        $end = strpos($this->buffer, "\r\n\r\n", 4);
+        if($end === false) return;
+
+        list($request, $headers) = $this->parse_header(substr($this->buffer, 0, $end));
+        list($version, $status, $phrase) = explode(' ', $request, 3);
+        $this->status = $status;
+        $this->response_length = (int)$headers['Content-Length'][0];
     }
 
-    $dir = dirname(dirname(dirname(__DIR__))).'/rackem.php';
-    $code = <<<EOT
+    /*
+    * Begin response for request.
+    */
+    public function process($app)
+    {
+        $spec = array(
+            0 => $this->body,
+            1 => array("pipe", "wb"),
+            2 => STDERR
+        );
+
+        $env = array();
+        // bring over standard $_SERVER array
+        foreach($_SERVER as $k=>$v) if(is_numeric($v) || is_string($v)) $env[$k] = $v;
+        $env = array_merge($env, $this->env);
+        foreach($this->header as $k=>$v) {
+            $env_name = str_replace("-", "_", strtoupper($k));
+            if(!isset($this->env[$env_name]))
+                $env["HTTP_$env_name"] = is_array($v) ? implode("\n", $v) : $v;
+        }
+
+        $dir = dirname(dirname(dirname(__DIR__))).'/rackem.php';
+        $code = <<<EOT
 if (!class_exists("\Rackem\Server")) require_once "$dir";
 if(function_exists("date_default_timezone_set")) date_default_timezone_set("UTC");
 require "$app";
 EOT;
 
-    $php = defined("PHP_BINARY") ? PHP_BINARY : PHP_BINDIR."/php";
-    $this->proc = proc_open("$php -r '$code'", $spec, $pipes, null, $env);
-    if(!is_resource($this->proc)) return false;
+        $php = defined("PHP_BINARY") ? PHP_BINARY : PHP_BINDIR."/php";
+        $this->proc = proc_open("$php -r '$code'", $spec, $pipes, null, $env);
+        if(!is_resource($this->proc)) return false;
 
-    stream_set_blocking($pipes[1], 0);
-
-    return $this->stream = $pipes[1];
-  }
+        $this->stream = $pipes[1];
+        stream_set_blocking($this->stream, 0);
+    }
 
 // private
-  protected function parse_request($raw)
-  {
-    $headers = explode("\r\n", $raw);
-
-    $this->request = array_shift($headers);
-
-    list($method, $uri, $version) = explode(' ', $this->request, 3);
-
-    $parsed_uri = parse_url($uri);
-
-    foreach($headers as $line)
+    protected function parse_header($raw)
     {
-      list($k, $v) = explode(": ", $line, 2);
-      if(!isset($this->header[$k]))
-        $this->header[$k] = array($v);
-      else
-        $this->header[$k][] = $v;
+        $headers = array();
+        $lines = explode("\r\n", $raw);
+
+        $request = array_shift($lines);
+
+        foreach($lines as $line) {
+            list($k, $v) = explode(": ", $line, 2);
+            if(!isset($headers[$k]))
+                $headers[$k] = array($v);
+            else
+                $headers[$k][] = $v;
+        }
+
+        return array($request, $headers);
     }
 
-    $scheme = 'http';
-    $host = $this->get_header('Host');
-    $port = 80;
-    if(strpos($host, ':') !== false)
+    protected function parse_request($raw)
     {
-      list($host, $port) = explode(':', $host);
-      $port = (int)trim($port);
-      if($port == 443) $scheme = 'https';
-    }
+        list($this->request, $this->header) = $this->parse_header($raw);
 
-    $this->env = array(
-      "CONTENT_LENGTH" => $this->content_length,
-      "CONTENT_TYPE" => $this->get_header('Content-Type'),
-      "REQUEST_METHOD" => $method,
-      "SCRIPT_NAME" => "",
-      "PATH_INFO" => $parsed_uri['path'],
-      "SERVER_NAME" => $host,
-      "SERVER_PORT" => $port,
-      "SERVER_PROTOCOL" => "HTTP/1.1",
-      "QUERY_STRING" => isset($parsed_uri['query']) ? $parsed_uri['query'] : '',
-      "rack_url_scheme" => $scheme,
-      "rackem_handler" => "rackem"
-    );
-  }
+        list($method, $uri, $version) = explode(' ', $this->request, 3);
+
+        $parsed_uri = parse_url($uri);
+
+        $scheme = 'http';
+        $host = $this->get_header('Host');
+        $port = 80;
+        if(strpos($host, ':') !== false) {
+            list($host, $port) = explode(':', $host);
+            $port = (int)trim($port);
+            if($port == 443) $scheme = 'https';
+        }
+
+        $this->env = array(
+            "CONTENT_LENGTH" => $this->content_length,
+            "CONTENT_TYPE" => $this->get_header('Content-Type'),
+            "REQUEST_METHOD" => $method,
+            "SCRIPT_NAME" => "",
+            "PATH_INFO" => $parsed_uri['path'],
+            "SERVER_NAME" => $host,
+            "SERVER_PORT" => $port,
+            "SERVER_PROTOCOL" => "HTTP/1.1",
+            "QUERY_STRING" => isset($parsed_uri['query']) ? $parsed_uri['query'] : '',
+            "rack_url_scheme" => $scheme,
+            "rackem_handler" => "rackem"
+        );
+    }
 
 }
