91b3212dbb17adfa03d05c717e7967311cacadbc
Mike Kruk <tamagokun@gmail.com>
2014-04-01 15:56:10 -0400

    Fixed preg_match bug in json_csrf
    preg_match conditional was going the wrong way!

--- lib/Rackem/Protection/JsonCsrf.php
+++ lib/Rackem/Protection/JsonCsrf.php
@@ -3,19 +3,25 @@ namespace Rackem\Protection;
 
 class JsonCsrf extends \Rackem\Protection
 {
-	public function call($env)
-	{
-		list($status,$headers,$body) = $this->app->call($env);
-		$content_type = isset($headers['Content-Type'])? explode(';',$headers['Content-Type'],2) : array('');
-		if(preg_match('/^\s*application\/json\s*$/',array_shift($content_type)) !== false)
-		{
-			$req = new \Rackem\Request($env);
-			if($this->referrer($env) != $req->host())
-			{
-				$result = $this->react($env);
-				$this->warn($env,"attack prevented by ".get_class($this));
-			}
-		}
+	
+    public function call($env)
+    {
+	    list($status,$headers,$body) = $this->app->call($env);
+	    $req = new \Rackem\Request($env);
+	    if ($this->has_vector($req, $headers)) {
+            $result = $this->react($env);
+            $this->warn($env, "attack prevented by ".get_class($this));
+        }
 		return isset($result)? $result : array($status,$headers,$body);
 	}
+
+    public function has_vector($req, $headers)
+    {
+        if ($req->is_xhr()) return false;
+
+		$content_type = isset($headers['Content-Type'])? explode(';',$headers['Content-Type'],2) : array('');
+		if(!preg_match('/^\s*application\/json\s*$/',array_shift($content_type))) return false;
+
+        return $this->referrer($req->env) != $req->host();
+    }
 }
